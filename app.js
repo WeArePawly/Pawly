c6a921de672b9b56fcd218f5e741511804de6c9a
miliberlin <michelle.liebheit@posteo.de>
2021-05-19 14:02:23 +0200

    edit app.js file to run build

--- app.js
+++ app.js
@@ -95,6 +95,15 @@ app.use("/api/vendors", vendors);
 const bookings = require("./routes/bookings");
 app.use("/api/bookings", bookings)
 
+// this is for the build
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
